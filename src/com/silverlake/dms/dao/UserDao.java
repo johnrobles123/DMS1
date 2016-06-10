180096083e6c9a0e36268b49aafa0b9ab9035594
john.robles <john.robles@silverlakeaxis.com>
2016-06-10 23:33:16 +0800

    Revert "Revert "Login work""
    This reverts commit 4ae5757c94276c129996c2ff7dc3c45a5ec6720c.

---
+++ src/com/silverlake/dms/dao/UserDao.java
@@ -0,0 +1,13 @@
+package com.silverlake.dms.dao;
+
+import java.sql.SQLException;
+
+/**
+ * @author CENTAUR
+ * This interface will be used to communicate with the
+ * Database
+ */
+public interface UserDao
+{
+		public boolean isValidUser(String username, String password) throws SQLException;
+}
