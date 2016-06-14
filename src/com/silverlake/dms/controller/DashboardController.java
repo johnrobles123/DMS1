1636ba660963db9d658c99bdf687e75e4a15147a
john.robles <john.robles@silverlakeaxis.com>
2016-06-14 11:27:00 +0800

    modified dashboard
    modified dashboard

--- src/com/silverlake/dms/controller/DashboardController.java
+++ src/com/silverlake/dms/controller/DashboardController.java
@@ -1,5 +1,7 @@
 package com.silverlake.dms.controller;
 
+import javax.ejb.EJB;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -10,17 +12,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.silverlake.dms.viewBean.DeviceBean;
+import com.silverlake.dms.repositories.DeviceJournalLocal;
+import com.silverlake.dms.repositories.entities.DeviceJournal;
 
 @Controller
-public class DashboardController {
+public class DashboardController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@EJB
+	private DeviceJournalLocal deviceJournal;
+	
+	public DashboardController() {
+		 
+    }
 
 	@RequestMapping(value="/dashboard",method=RequestMethod.GET)
-	public ModelAndView displayDashboard(HttpServletRequest request, HttpServletResponse response, DeviceBean deviceBean)
+	public ModelAndView displayDashboard(HttpServletRequest request, HttpServletResponse response, DeviceJournal deviceJournal)
 	{
 		ModelAndView model = new ModelAndView("dashboard");
 		//LoginBean loginBean = new LoginBean();
-		model.addObject("deviceBean", deviceBean);
+		
+		List<DeviceJournal> deviceJournalList = new ArrayList<DeviceJournal>();
+		
+		model.addObject("deviceJournal", deviceJournal);
 		return model;
 	}
 }
